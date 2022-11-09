a57abb766ce0f0e94f593e7b76e326648b363c97
kundaroger <byakundaroger@gmail.com>
2022-11-09 12:46:31 +0200

    added index, today, mynames pages

---
+++ api.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request
+from datetime import datetime
+
+storage = FastAPI(title="MY FASTAPI")
+
+
+
+@storage.get('/')#Route
+def index():
+    return "My name is Roger, This my API"
+
+@storage.get('/today')#route with GET METHOD
+def today():
+    return str(datetime.now())
+
+
+@storage.get('/mynames')
+def names(first_name: bool = False, last_name: bool = False, full_name: bool = False):
+    full_names = ""
+    if first_name:
+        full_names += 'Roger'
+    if last_name:
+        full_names += ' Byakunda'
+    if full_name:
+        full_names = "Roger Byakunda"
+    
+    return full_names
